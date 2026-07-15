6eb2c5d501afeb12d155f86808c6544c01da2d3a
Logan Edwards <ledwards@robellum.net>
2026-07-15 17:53:03 -0500

    Remove redundant dependencies on nvim config live_preview.lua
    Only needed one picker

--- nvim/lua/plugins/live_preview.lua
+++ nvim/lua/plugins/live_preview.lua
@@ -1,10 +1,6 @@
 return {
   "brianhuster/live-preview.nvim",
   dependencies = {
-    -- You can choose one of the following pickers
-    "nvim-telescope/telescope.nvim",
-    "ibhagwan/fzf-lua",
-    "nvim-mini/mini.pick",
     "folke/snacks.nvim",
   },
 }
